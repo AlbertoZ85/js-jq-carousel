9ed0e4841f87e0d7c7bebd1cfc1c36bc79905a80
Alberto Zordan <zorberto@gmail.com>
2020-07-23 15:29:15 +0200

    init

---
+++ main.js
@@ -0,0 +1,10 @@
+// Creare uno slider di immagini
+// Potete usare le immagini che desiderate.
+// Per facilitarvi la vita usate immagini delle stesse dimensioni :)
+// Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider
+// Utilizziamo una classe first e last  per capire quali sono la prima e ultima immagine dello slider
+// Utilizziamo una classe active per aiutarci a capire qual è l’immagine attuale da visualizzare nello slider
+//
+// Bonus:
+// a. Clicchiamo sui pallini e mostriamo l’immagine corrispondente. Generiamo i pallini con JS
+// b. Per scorrere le immagini permettere anche l'uso delle frecce sinistra e destra della tastiera
