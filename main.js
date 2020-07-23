f0b2cbdfe3fc96e758861afcaf8b649dee3d679b
Alberto Zordan <zorberto@gmail.com>
2020-07-23 16:03:47 +0200

    Done! (without bonus)

--- main.js
+++ main.js
@@ -8,3 +8,54 @@
 // Bonus:
 // a. Clicchiamo sui pallini e mostriamo l’immagine corrispondente. Generiamo i pallini con JS
 // b. Per scorrere le immagini permettere anche l'uso delle frecce sinistra e destra della tastiera
+
+// 1. Uso il metodo .ready() per assicurarmi che il codice js venga eseguito soltanto a caricamento del DOM ultimato
+$(document).ready(function() {
+    // 2. al click delle frecce sx e dx invoco le rispettive funzioni per lo scorrimento delle immagini e dei relativi pallini della barra di navigazione.
+    $('.prev').click(prevImg);
+    $('.next').click(nextImg);
+    // In questo caso le funzioni vengono invocate senza parentesi () in quanto assumono la valenza di argomenti assegnati come parametri al metodo .click() (metodo = funzione applicata a un oggetto)
+});
+
+// *** FUNZIONI *** //
+// a. Funzione che al click della freccia sx del carosello visualizza l'immagine precedente e accende il relativo pulsante di navigazione
+function prevImg() {
+    // salvo gli elementi che possiedono la classe 'active' in delle variabili LOCALI (poiché uso la stessa variabile nella funzione nextImg e quindi non devo generare potenziali conflitti)
+    var activeImg = $('.images img.active');
+    var activeNavDot = $('.nav i.active');
+
+    // rimuovo dapprima la classe 'active' da tali elementi, in modo da poterla attribuire successivamente all'elemento desiderato dopo aver effettuato il controllo sulla sua posizione
+    activeImg.removeClass('active');
+    activeNavDot.removeClass('active');
+
+    // check sulla posizione dell'elemento attivo corrente: se è il primo, allora attribuisco la classe 'active' all'ultimo; idem per il pallino
+    if (activeImg.hasClass('first')) {
+        $('.images img.last').addClass('active');
+        $('.nav i.last').addClass('active');
+    }   else {
+        // altrimenti attribuisco la classe 'active' all'elemento precedente a quello corrente
+        activeImg.prev().addClass('active');
+        activeNavDot.prev().addClass('active');
+    }
+}
+
+// b. Funzione che al click della freccia dx del carosello visualizza l'immagine successiva e accende il relativo pulsante di navigazione
+function nextImg() {
+    // salvo in delle variabili LOCALI gli elementi che possiedono la classe 'active'
+    var activeImg = $('.images img.active');
+    var activeNavDot = $('.nav i.active');
+
+    // rimuovo dapprima la classe 'active' da tali elementi, in modo da poterla attribuire successivamente all'elemento desiderato dopo aver effettuato il controllo sulla sua posizione
+    activeImg.removeClass('active');
+    activeNavDot.removeClass('active');
+
+    // check sulla posizione dell'elemento attivo corrente: se è l'ultimo, allora attribuisco la classe 'active' al primo; idem per il pallino
+    if (activeImg.hasClass('last')) {
+        $('.images img.first').addClass('active');
+        $('.nav i.first').addClass('active');
+    }   else {
+        // altrimenti attribuisco la classe 'active' all'elemento successivo a quello corrente
+        activeImg.next().addClass('active');
+        activeNavDot.next().addClass('active');
+    }
+}
